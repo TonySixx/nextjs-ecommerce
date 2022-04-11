3facf5ffecf9d66db13061c2da74aa6ec4231d55
TonySixx <75792327+TonySixx@users.noreply.github.com>
2022-04-11 16:18:45 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-        "BASE_URL": "/",
+        "BASE_URL": "",
         "MONGODB_URL": "mongodb+srv://admin:db-devwars@cluster0.z1x1o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
         "ACCESS_TOKEN_SECRET": "abcd",
         "REFRESH_TOKEN_SECRET": "cbda",
